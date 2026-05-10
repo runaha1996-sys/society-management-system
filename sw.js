caf8ebb0271ce6a8bccbe728c89168e074743a00
Pranjal Mundra <pranjalmundra02@gmail.com>
2026-05-11 02:34:54 +0530

    Add PWA support for mobile app experience

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'aananda-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/login.html',
+  '/dashboard.html',
+  '/css/style.css',
+  '/js/app.js',
+  '/images/app_icon.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Strategy: Network First, falling back to Cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
